08dc4a85fdb02a76b620ca38dd001caaf642010b
Faizur Rahman Khan <rahmankhan1113@gmail.com>
2024-06-18 00:10:46 +0530

    Removed unwanted codes from Homepage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -6,20 +6,4 @@ import utility.BrowserDriver;
 
 public class HomePage extends BrowserDriver {
 
-    public static String hamburger_menu_xpath = "//input[@type='checkbox']";
-    public static String sign_in_option = "//a[@href='SignIn.html']";
-
-    public static void click_Hamburger_Menu() throws InterruptedException{
-        driver.manage().window().maximize();
-        driver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
-        Thread.sleep(2000);
-        driver.findElement(By.xpath(hamburger_menu_xpath)).click();
-    }
-
-    public static void click_signIn_Link() throws InterruptedException {
-        Thread.sleep(2000);
-        driver.findElement(By.xpath(sign_in_option)).click();
-        Thread.sleep(2000);
-        driver.close();
-    }
 }
