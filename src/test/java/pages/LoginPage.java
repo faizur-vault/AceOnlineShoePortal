a7f1cef67b14a2763238bd175738f1dab45d3552
Faizur Rahman Khan <rahmankhan1113@gmail.com>
2024-06-25 20:44:50 +0530

    Added some waits and steps to maximize windows

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -14,15 +14,18 @@ public class LoginPage extends BrowserDriver {
         driver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
         Thread.sleep(2000);
         driver.findElement(By.xpath(hamburger_menu_xpath)).click();
+        System.out.println("Clicked on Hamburger menu");
     }
 
     public static void click_signIn_Link() throws InterruptedException {
         Thread.sleep(2000);
         driver.findElement(By.xpath(sign_in_option)).click();
         Thread.sleep(4000);
+        System.out.println("Clicked on Sign in");
     }
 
     public static void click_signIn_Link2() throws InterruptedException {
         driver.close();
+        System.out.println("Closed Driver");
     }
 }
